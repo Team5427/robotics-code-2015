ec6e4406d30a3af4c35c5abdfb142d459ddce762
Bo Corman <bo.corman@gmail.com>
2015-01-04 16:02:11 -0600

    did lots of work

--- Robotics/src/java/test.java
+++ Robotics/src/java/test.java
@@ -1,11 +1,15 @@
 
 public class test {
 	public static void main(String[] args){
-		System.out.println("didst thou recieve thine update to the bucket of bits?");
+		System.out.println("did you recieve thine update to the bucket of bits?");
 		System.out.println("Oui!!!");
+		//changed a bit here
 		System.out.println("i believe i can push!");
 		System.out.println("no par le francias");		//IWANNAPUSH
 		System.out.println("I will speak in french !!!");
+		//I like cheese
+		//lots of work 
+		
 
 	}
 }
