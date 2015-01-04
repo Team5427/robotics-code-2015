859cf9cf0f4a62e1166b656ba2a6b5239eb619d3
Bo Corman <bo.corman@gmail.com>
2015-01-04 15:17:51 -0600

    TESTING

--- Robotics/src/java/test.java
+++ Robotics/src/java/test.java
@@ -3,5 +3,6 @@ public class test {
 	public static void main(String[] args){
 		System.out.println("didst thou recieve thine update to the bucket of bits?");
 		System.out.println("Oui!!!");
+		System.out.println("lkasjdfklasj;dfklj");
 	}
 }
