02f3af8c42c1c64243cd767f38ffdcc452a0e42c
Sudarshan <Sudarshan@10.68.9.94>
2015-01-04 14:57:29 -0600

    lol

--- Robotics/src/java/test.java
+++ Robotics/src/java/test.java
@@ -2,5 +2,6 @@
 public class test {
 	public static void main(String[] args){
 		System.out.println("didst thou recieve thine update to the bucket of bits?");
+		System.out.println("Oui!!!");
 	}
 }
