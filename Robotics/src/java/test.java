3c24eee3ca5e280431abc797145d4684d623a434
andrew <andrewkennedyftw@gmail.com>
2015-01-04 16:28:20 -0600

    horseplay

--- Robotics/src/java/test.java
+++ Robotics/src/java/test.java
@@ -1,16 +1,6 @@
 
 public class test {
 	public static void main(String[] args){
-		System.out.println("did you recieve thine update to the bucket of bits?");
-		System.out.println("Oui!!!");
-		//changed a bit here
-		System.out.println("i believe i can push!");
-		System.out.println("no par le francias");		//IWANNAPUSH
-		//and here
-		System.out.println("#WHAP GOD!!!");//said Andrew
-		System.out.println("I will speak in french !!!");
-		//CHANGED FIRST
-		//This deleted all of bo's work
-
+		//as mr. Devlin says, no HORSE PLAY!!! somebody could get seriously hurt!
 	}
 }
