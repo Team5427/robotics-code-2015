16f005e173a954b6d70482acd723ef493553829d
andrew <andrewkennedyftw@gmail.com>
2015-01-27 19:05:16 -0600

    button now works

--- dual joystick mecanum drive/src/org/usfirst/frc/team5427/robot/DriveTrain.java
+++ dual joystick mecanum drive/src/org/usfirst/frc/team5427/robot/DriveTrain.java
@@ -24,66 +24,60 @@ public class DriveTrain
 //		Drive d = new Drive();
 		while(true){
 		if(j.getRawButton(2) == true){
-		fr.set(1);
+			double x = j.getX();
+//			if(x !=0)
+//				continue;
+		if(x >0){
+			fl.set(x);
+			rl.set(-x);
+			fr.set(x);
+			rr.set(-x);
 		}
+		
+		else if(x<0){	
+			fl.set(x);
+			rl.set(-x);
+			fr.set(x);
+			rr.set(-x);
+	}	
+		}
+		double y = j.getY();
+		double z = j.getZ();
+		double left = 0;
+		double right = 0;
+		boolean b = false;
+		if(z>=0){
+			z = 1-z;
+			b = true;
+		}
+		else{
+			z = z+1;
+			b = false;
+		}
+		if(b){
+			right = y*z;
+			left = y;
+		}else{
+			left = y*z;
+			right = y;
+		}
+
+		fl.set(-left);
+		rl.set(-left);
+		fr.set(right);
+		rr.set(right);
+		
+	
+
+		
 		}
 			/*
-        for(int i = 0; i<50; i++){
-        	rl.set(.5);
-        	rr.set(-.5);
-        	fr.set(-.5);
-        	fl.set(.5);
-        }
-		
+	
 		while(true){
 			try
 			{
-		//		toggle.whileHeld(d);
-			//toggle.whenReleased();
-				/* Code for a basic tank drive
-				double y = j.getY();
-				double z = j.getZ();
-				double left = 0;
-				double right = 0;
-				boolean b = false;
-				if(z>=0){
-					z = 1-z;
-					b = true;
-				}
-				else{
-					z = z+1;
-					b = false;
-				}
-				if(b){
-					right = y*z;
-					left = y;
-				}else{
-					left = y*z;
-					right = y;
-				}
 
-				fl.set(left);
-				rl.set(left);
-				fr.set(right);
-				rr.set(right);
-				
-			
-				double x = j.getX();
-//					if(x !=0)
-//						continue;
-				if(x >0){
-					fl.set(x);
-					rl.set(-x);
-					fr.set(x);
-					rr.set(-x);
-				}
-				
-				else if(x<0){	
-					fl.set(x);
-					rl.set(-x);
-					fr.set(x);
-					rr.set(-x);
-			}			
+				/* Code for a basic tank drive
 				
 				
 				Thread.sleep(5);
