022916639ad71c1c8517af7903301fa4a948c24e
Bo Corman <bo.corman@gmail.com>
2015-02-08 16:14:05 -0600

    added config -- not using yet

--- dual joystick mecanum drive/src/org/usfirst/frc/team5427/robot/Config.java
+++ dual joystick mecanum drive/src/org/usfirst/frc/team5427/robot/Config.java
@@ -1,5 +1,34 @@
 package org.usfirst.frc.team5427.robot;
 
 public class Config {
+	
+	//Ports
 	public static final int frontLeftPort = 0;
+	public static final int frontRightPort = 1;
+	public static final int rearLeftPort = 2;
+	public static final int rearRightPort = 3;
+	public static final int liftPort = 4;
+	public static final int armPort = 5;
+	
+	//Direction
+	public static final boolean frontLeftIsReverse = true;
+	public static final boolean frontRightIsReverse = false;
+	public static final boolean rearLeftIsReverse = true;
+	public static final boolean rearRightIsReverse = false;
+	
+	//Offsets
+	public static final double frontLeftForwardOffset = .0333;
+	public static final double frontLeftBackwardOffset = .0333;
+	public static final double frontRightForwardOffset = 0;
+	public static final double frontRightBackwardOffset = 0;
+	public static final double rearLeftForwardOffset = .03;
+	public static final double rearLeftBackwardOffset = .03;
+	public static final double rearRightForwardOffset = .003;
+	public static final double rearRightBackwardOffset = .003;
+	
+	//Arm and lift speeds
+	public static final double armSpeed = .5;
+	public static final double liftSpeed = .5;
+	
+	
 }
